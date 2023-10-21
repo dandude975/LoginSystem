7c49ddea086b7de8bfc21b0d6b1aaba4084ebfe8
Dan <93231187+dandude975@users.noreply.github.com>
2023-10-21 07:38:10 +0100

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <String>
+using namespace std;
+
+
+int main() {
+    cout << "Please enter login Credentials" << endl;
+    cout << "Username:";
+    string Uname;
+    string Pword;
+    cin >> Uname;
+    cout << "Password:";
+    cin >> Pword;
+    cout << "\nUsername:" << Uname << "\nPassword:" << Pword << endl;
+    return 0;
+}
